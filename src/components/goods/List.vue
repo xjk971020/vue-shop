04f5be4697875cd63958c408baaf6956a12592e7
xjk971020 <823840954@qq.com>
2020-01-27 17:31:42 +0800

    删去商品列表编辑商品功能

--- src/components/goods/List.vue
+++ src/components/goods/List.vue
@@ -43,9 +43,6 @@
         </el-table-column>
         <el-table-column label="操作">
           <template slot-scope="scope">
-            <el-button type="primary" icon="el-icon-edit" size="small"
-              >编辑</el-button
-            >
             <el-button
               type="danger"
               icon="el-icon-delete"
